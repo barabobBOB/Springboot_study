e60008af8714dc75e55089e09d688cc8702e2f07
choiseyeon <choi20014830@gmail.com>
2022-02-12 03:10:26 +0900

    빌드오류 수정 + 기본틀

--- hello-spring/src/main/java/hello/hellospring/Controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package hello.hellospring.Controller;
+package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
-    @GetMapping("hello") //get 메서드
-    public String hello(Model model){
-        model.addAttribute("data", "Hello!!");//키는 데이터, 값은 헬로
-        return "hello"; // Thy머시기 템플릿엔진 hello로 실행 시켜라
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
     }
 }
